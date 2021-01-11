486fee8a45906139b66f1fb515d80d343117f9cd
hdj <hdj@addcn.com>
2021-01-11 08:12:23 +0800

    first commit

---
+++ .phpstorm.meta.php
@@ -0,0 +1,20 @@
+<?php
+
+// @formatter:off
+
+declare(strict_types=1);
+/**
+ * This file is part of mall-service.
+ *
+ * @link     https://code.addcn.com/8591/services/mall
+ * @document https://code.addcn.com/8591/services/mall/blob/master/README.md
+ * @contact  hdj@addcn.com
+ */
+namespace PHPSTORM_META;
+
+    // Reflect
+    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+    override(\Hyperf\Utils\Context::get(0), map(['' => '@']));
+    override(\make(0), map(['' => '@']));
+    override(\app(0), map(['' => '@']));
+    override(\di(0), map(['' => '@']));
---
+++ src/helpers.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of hyperf/helpers.
+ *
+ * @link     https://github.com/friendsofhyperf/helpers
+ * @document https://github.com/friendsofhyperf/helpers/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+if (! function_exists('app')) {
+    /**
+     * @throws TypeError
+     * @return mixed|\Psr\Container\ContainerInterface
+     */
+    function app(string $abstract = null, array $parameters = [])
+    {
+        if (\Hyperf\Utils\ApplicationContext::hasContainer()) {
+            $container = \Hyperf\Utils\ApplicationContext::getContainer();
+
+            if (is_null($abstract)) {
+                return $container;
+            }
+
+            if (count($parameters) == 0 && $container->has($abstract)) {
+                return $container->get($abstract, $parameters);
+            }
+
+            if (method_exists($container, 'make')) {
+                return call_user_func_array([$container, 'make'], [$abstract, array_values($parameters)]);
+            }
+        }
+
+        return new $abstract(...array_values($parameters));
+    }
+}
+
+if (! function_exists('when')) {
+    /**
+     * @param mixed $expr
+     * @param mixed $value
+     * @param mixed $default
+     * @return mixed
+     */
+    function when($expr, $value = null, $default = null)
+    {
+        $result = value($expr) ? $value : $default;
+
+        return $result instanceof \Closure ? $result($expr) : $result;
+    }
+}
+
+if (! function_exists('info')) {
+    /**
+     * @param string $message
+     * @throws TypeError
+     */
+    function info($message, array $context = [])
+    {
+        return logs()->info($message, $context);
+    }
+}
+
+if (! function_exists('logger')) {
+    /**
+     * @param null|string $message
+     * @throws TypeError
+     * @return \Psr\Log\LoggerInterface|void
+     */
+    function logger($message = null, array $context = [])
+    {
+        if (is_null($message)) {
+            return logs();
+        }
+
+        return logs()->debug($message, $context);
+    }
+}
+
+if (! function_exists('logs')) {
+    /**
+     * @param string $name
+     * @param string $group
+     * @throws TypeError
+     * @return \Psr\Log\LoggerInterface
+     */
+    function logs($name = 'hyperf', $group = 'default')
+    {
+        return app(\Hyperf\Logger\LoggerFactory::class)->get($name, $group);
+    }
+}
