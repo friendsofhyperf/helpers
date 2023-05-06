d82af9249e3237afb490e6c20420d65e88265bef
Deeka Wong <huangdijia@gmail.com>
2023-05-06 12:23:45 +0800

    Adds `support` component (#196)
    * Adds `support` component
    
    * ⚙️chore(dependencies) add hyperf/di and hyperf/context to composer.json
    
    * ⚙️chore(license) update copyright info
    
    * ⚙️chore(dependencies) add carbon package to composer.json
    
    * Adds Timebox
    
    * Adds test cases
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- src/Foundation/Environment.php
+++ src/Foundation/Environment.php
@@ -10,74 +10,9 @@ declare(strict_types=1);
  */
 namespace FriendsOfHyperf\Helpers\Foundation;
 
-use Hyperf\Macroable\Macroable;
-use Hyperf\Stringable\Str;
-
-use function Hyperf\Support\env;
-
-class Environment
+/**
+ * @deprecated v3.1, use `\FriendsOfHyperf\Support\Environment` instead.
+ */
+class Environment extends \FriendsOfHyperf\Support\Environment
 {
-    use Macroable;
-
-    public function __construct(protected ?string $env = null)
-    {
-        $this->env = $env ?? env('APP_ENV');
-    }
-
-    /**
-     * Get or check the current application environment.
-     *
-     * @param array|string $environments
-     * @return bool|string
-     */
-    public function environment(...$environments)
-    {
-        if (count($environments) > 0) {
-            $patterns = is_array($environments[0]) ? $environments[0] : $environments;
-
-            return Str::is($patterns, $this->env);
-        }
-
-        return $this->env;
-    }
-
-    /**
-     * Determine if the application is in the local environment.
-     */
-    public function isLocal(): bool
-    {
-        return $this->environment('local');
-    }
-
-    /**
-     * Determine if the application is in the dev environment.
-     */
-    public function isDev(): bool
-    {
-        return $this->environment('dev');
-    }
-
-    /**
-     * Determine if the application is in the develop environment.
-     */
-    public function isDevelop(): bool
-    {
-        return $this->environment('develop');
-    }
-
-    /**
-     * Determine if the application is in the production environment.
-     */
-    public function isProduction(): bool
-    {
-        return $this->environment('production');
-    }
-
-    /**
-     * Determine if the application is in the production environment.
-     */
-    public function isOnline(): bool
-    {
-        return $this->environment('online');
-    }
 }
